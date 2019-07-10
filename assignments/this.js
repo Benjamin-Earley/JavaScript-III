83be7ceeb59a9c8ef1213322113d5025faaf5058
Benjamin Earley <benjaminearley@Benjamins-MacBook-Pro-2.local>
2019-07-09 21:43:28 -0400

    this.js complete.

--- assignments/this.js
+++ assignments/this.js
@@ -1,4 +1,4 @@
-/* The for principles of "this";
+/* The four principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. Window/Global Binding: Unless provided with an object to reference, 'this' will simply refer to the window/console object in its entirety.
@@ -12,16 +12,48 @@
 // Principle 1
 
 // code example for Window Binding
-
+function globalFunction() {
+    console.log("***'this' in Global Binding:\n", this);
+  }
+  globalFunction();
+  
 // Principle 2
 
 // code example for Implicit Binding
+const object1 = {
+    name: "Ben",
+    age: 29,
+    likesTo: function(activity) {
+        console.log("***'this' in Implicit Binding:\n", this);
+        console.log(`Hi! I'm ${this.name}, I'm ${this.age}, and I like to ${activity}.`);
+    }
+};
+object1.likesTo("run");
 
 // Principle 3
 
 // code example for New Binding
+function Hobbies(person, hobby) {
+    this.name = person;
+    this.hobby = hobby;
+    this.enjoys = function() {
+        console.log(`${this.name} loves to ${this.hobby}.`);
+    };
+    console.log("***'this' in New Binding:\n", this);
+}
+const james = new Hobbies("James", "skate");
+const lisa = new Hobbies("Lisa", "play soccer");
+const chuan = new Hobbies("Chuan", "sing");
+
+james.enjoys();
+lisa.enjoys();
+chuan.enjoys();
 
 // Principle 4
 
 // code example for Explicit Binding
-
+console.log("***'this' in Explicit Binding:\n");
+console.log("lisa.enjoys() using .call(james)");
+    lisa.enjoys.call(james);
+console.log("chuan.enjoys() using .apply(lisa)");
+    chuan.enjoys.apply(lisa);
